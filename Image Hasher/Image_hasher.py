3c07a9b492d845e76e0bef36b8c601bb0b217469
roganjoshu <56878631+roganjoshu@users.noreply.github.com>
2020-12-10 21:11:46 +0000

    Hash fix
    Fixed issue where 1st hash calculated was being multiplied by 2 for each hash following the original.

--- Image Hasher/Image_hasher.py
+++ Image Hasher/Image_hasher.py
@@ -7,9 +7,8 @@ from Image import Image
 from PIL import Image as image
 
 substring = ".ini"
-path_to_file = "C:\\Users\\Josh\\Pictures\\Saved Pictures"
+path_to_file = "D:\\testData"
 images = list()
-duplicates = list()
 
 #read images in from path, convert to mat obejct and store in custom type Image
 def read_images(path_contents, path_to_file):
@@ -35,25 +34,26 @@ def read_images(path_contents, path_to_file):
 #9x8 because 9 pixels compared against 8 adjacent pixels renders 8x8 64bit array
 def hash_image(images):
 
-    hashsize = 8
-    image_hash = 0
     for index, image in enumerate(images):
-        grayscale = cv.cvtColor(image.get_image(), cv.COLOR_BGR2GRAY)
-        image_Resized = cv.resize(grayscale, (hashsize + 1, hashsize))
-        r, c = image_Resized.shape
-        pixel_difference = np.ndarray(shape=(r,c-1), dtype=bool)
+        hashsize = 8
+        image_hash = 0
 
-        for row in range(r):
-            for col in range(c-1):
-                if image_Resized[row,col+1] > image_Resized[row,col]:
-                    pixel_difference[row,col] = True
-                else:
-                    pixel_difference[row,col] = False
+        grayscale = cv.cvtColor(image.get_image(), cv.COLOR_BGR2GRAY)  
+        image_resized = cv.resize(grayscale, (hashsize + 1, hashsize))
+        m, n = image_resized.shape
+        pixel_difference = np.ndarray(shape=(m, n-1), dtype=bool)
 
-
+        for row in range(m):
+                for col in range(n-1):
+                    if image_resized[row, col+1] > image_resized[row, col]:
+                        pixel_difference[row,col] = True
+                    else:
+                        pixel_difference[row,col] = False
+        
         for index, value in enumerate(pixel_difference.flatten()):
-            if value == True:
-                image_hash += 5**index
+                if value == True:
+                    image_hash += 5**index
+
         image.set_hash(image_hash)
 
 #if hashes & channels/size the same then a duplicate has been found
@@ -66,24 +66,6 @@ def identify_duplicate_hashes(images):
             elif new_image.get_hash() == image.get_hash() and new_image.get_image_shape() == image.get_image_shape():
                 print(new_image.get_name())
 
-
-def binary_search_hashes(images, size, query_hash, image_index):
-
-    low = 0
-    high = size - 1
-    while low <= high:
-
-        mid = int(low + (high - low) / 2)
-        if query_hash == images[mid].get_hash() and image_index != mid:
-            return True
-        elif query_hash < images[mid].get_hash():
-            high = mid - 1
-        else:
-            low = mid + 1
-    return False
-
-
-
 #call functions, read images, hash images, identify duplicate hashes/images
 time1 = time()
 
@@ -91,8 +73,8 @@ path_contents = os.listdir(path_to_file)
 read_images(path_contents, path_to_file)
 hash_image(images)
 identify_duplicate_hashes(images)
-#images.sort(key=lambda x: x.get_hash(), reverse=False)
 
 time2 = time()
 time_taken = time2 - time1
+
 print(str(time_taken))
