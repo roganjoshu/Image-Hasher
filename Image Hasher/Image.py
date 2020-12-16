a372134d3c721bd2149476f987341a5d40638e8d
roganjoshu <56878631+roganjoshu@users.noreply.github.com>
2020-12-16 21:41:48 +0000

    implemented binary search to check for number of times an image appears

--- Image Hasher/Image.py
+++ Image Hasher/Image.py
@@ -9,6 +9,7 @@ class Image:
         else:
             self.date_created = 0
         self.image_shape = image_shape
+        self.is_duplicate_tag = False
 
     # getters
     def get_name(self):
@@ -19,9 +20,13 @@ class Image:
         return self.image
     def get_image_shape(self):
         return self.image_shape
+    def get_tag(self):
+        return self.is_duplicate_tag
 
     # setters
     def set_hash(self, image_hash):
         self.image_hash = image_hash
     def set_image(self, image):
         self.image = image
+    def set_tag(self, is_duplicate_tag):
+        self.is_duplicate_tag = duplicate_tag
--- Image Hasher/Image_hasher.py
+++ Image Hasher/Image_hasher.py
@@ -7,13 +7,13 @@ from Image import Image
 from PIL import Image as image
 import vptree
 
-
+#C:\\Users\\Josh\\Pictures\\duplicates - 5
 #C:\\Users\\Josh\\Pictures\\Saved Pictures - 15 items
 #D:\/S20 Pics - 97 items
 #D:\\Univeristy\\3rd Year\\Honours Stage Project\\archive\\chest_xray\\chest_xray\\test\\NORMAL - 234 items
 #D:\\Univeristy\\3rd Year\\Honours Stage Project\\archive\\chest_xray\\train\\PNEUMONIA - 3875 items
 substring = ".ini"
-path_to_file = "D:\\Univeristy\\3rd Year\\Honours Stage Project\\archive\\chest_xray\\chest_xray\\test\\NORMAL"
+path_to_file = "D:\\Univeristy\\3rd Year\\Honours Stage Project\\archive\\chest_xray\\train\\PNEUMONIA"
 images = list()
 
 #read images in from path > create customt type > hash image > append to list
@@ -77,45 +77,42 @@ def hash_image(image, temp_image):
 
     return image_hash
 
-#if hashes & channels/size the same then a duplicate has been found
-#O(n) linear time complexity, very slow for larger datasets (to be defined)
-def identify_duplicate_hashes(images):
-    count = 0
-    print("Idenitfying duplicates")
-    for index, image in enumerate(images):
-        for new_index, new_image in enumerate(images):
-            if new_index <= index:
-                continue
-            elif new_image.get_hash() == image.get_hash() and new_image.get_image_shape() == image.get_image_shape():
-                print(new_image.get_name())
-                print(image.get_name())
-                count += 1
-    print(count)
-
 #this needss checking
-def recursive_binary_search(images, low, high, hash_to_check):
-    if low > high:
-        return False
-    mid = int(low + (high - low) / 2)
-
-    if hash_to_check == images[mid].get_hash():
-        return True
-    elif hash_to_check < images[mid].get_hash():
-        return recursive_binary_search(images, low, mid - 1, hash_to_check)
-    else:
-        return recursive_binary_search(images, mid + 1, high, hash_to_check)
-
-
-
+def binary_search(images, size, image, search_first):
+    low = 0
+    high = size - 1
+    result = -1
+    while low <= high:
+        mid = int((low + high) / 2)
+        if image.get_hash() == images[mid].get_hash():
+            result = mid
+            if search_first:               
+                high = mid - 1  #search left/ lower indices
+            else:
+                low = mid + 1   #search right/ higher indices
+        elif image.get_hash() < images[mid].get_hash():
+            high = mid - 1
+        else:
+            low = mid + 1
+    return result
 
 #call functions, read images, hash images, identify duplicate hashes/images
 time1 = time()
 
 path_contents = os.listdir(path_to_file)
 read_images(path_contents, path_to_file)
-identify_duplicate_hashes(images)
 
-images.sort(key=lambda x: x.get_hash(), reverse=True)
+images.sort(key=lambda x: x.get_hash(), reverse=False)
+
+#check for number of times an image appears
+for index, image in enumerate(images):
+
+    firstIndex = binary_search(images, len(images), image, True)
+    if firstIndex == -1:
+        print("no duplicates")
+    else:
+        lastIndex = binary_search(images, len(images), image, False)
+        print(lastIndex - firstIndex +1)
 
 time2 = time()
 time_taken = time2 - time1
